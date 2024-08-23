1f1535234960b40d339d50e9fe5d67f37b82be56
Hiroyuki Miyazaki <hiroyukimiyazaki@HiroyukinoMacBook-Air.local>
2024-08-23 18:20:45 +0900

    save

--- start-docker-componses.sh
+++ start-docker-componses.sh
@@ -5,9 +5,18 @@ base_path="common/ch-configs"
 # Function to start docker-compose for each service
 start_docker_compose() {
   local compose_file=$1
-  echo $compose_file
+  echo "Starting service with $compose_file"
   docker-compose -f $compose_file up -d
 }
+
+# Start the redis service
+# redis_compose_file="$base_path/compose.redis.yml"
+# if [ -f "$redis_compose_file" ]; then
+#   start_docker_compose "$redis_compose_file"
+# else
+#   echo "Redis compose file not found: $redis_compose_file"
+#   exit 1
+# fi
 
 # Start the root service
 start_docker_compose "$base_path/compose.server.yml"

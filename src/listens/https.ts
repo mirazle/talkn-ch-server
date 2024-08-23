71f0446ec7bd93f0f876afb19630daf8b13a8932
Hiroyuki Miyazaki <hiroyukimiyazaki@HiroyukinoMacBook-Air.local>
2024-08-23 17:08:59 +0900

    save

--- src/listens/https.ts
+++ src/listens/https.ts
@@ -1,14 +1,13 @@
 import https from 'https';
 
 import conf from '@server/conf';
-import ChModel, { Connection } from '@common/models/Ch';
+import { Connection } from '@common/models/Ch';
 import { ChConfig } from '@common/models/ChConfig';
 
 const { io, ssl } = conf;
 
 const getHttpsServer = async (topConnection: Connection, myChConfig: ChConfig): Promise<https.Server> => {
-  const isRootConnection = topConnection === ChModel.rootConnection;
-  const ioPort = isRootConnection ? Number(io.root.port) : Number(myChConfig?.nginx.port);
+  const ioPort = Number(myChConfig?.nginx.port);
   return new Promise((resolve, reject) => {
     const httpsServer = https.createServer(ssl);
     httpsServer
